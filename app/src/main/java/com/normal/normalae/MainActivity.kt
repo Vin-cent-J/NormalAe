f025141f67d55c4e0623c22d0a88265c2cad8e14
Vin-cent-J <vincent.joewono123@gmail.com>
2023-12-25 20:32:35 +0700

    Login

--- app/src/main/java/com/normal/normalae/MainActivity.kt
+++ app/src/main/java/com/normal/normalae/MainActivity.kt
@@ -3,7 +3,12 @@ package com.normal.normalae
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.normal.normalae.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,13 +22,26 @@ class MainActivity : AppCompatActivity() {
 
         bind.btnSignin.setOnClickListener {
             var user = User(bind.txtPassword.text.toString(), bind.txtUsername.text.toString())
-            if(Global.users.contains(user)){
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
+            val q = Volley.newRequestQueue(this)
+            val url = "https://ubaya.me/native/160421053/login.php"
+            val stringRequest = object : StringRequest(
+                Request.Method.POST, url,
+                Response.Listener {
+                    Log.d("cekparams", it)
+                },
+                Response.ErrorListener {
+                    Log.d("cekparams", it.message.toString())
+                })
+            {
+                override fun getParams(): MutableMap<String, String> {
+                    val params = HashMap<String, String>()
+                    params["username"] = bind.txtUsername.text.toString()
+                    params["password"] = bind.txtPassword.text.toString()
+
+                    return params
+                }
             }
-            else{
-                Toast.makeText(this, "Wrong password or username.", Toast.LENGTH_SHORT).show()
-            }
+            q.add(stringRequest)
         }
         bind.btnSignup2.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
